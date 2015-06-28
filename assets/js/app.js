4d2c207c4240eeab0d6d27a09b2ec630bc9e5134
qhu5 <quentinhu5@gmail.com>
2015-06-28 22:21:05 +0000

    update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,52 @@
 var React = require("react");
 var App;
+var clc = {
+  A: {
+    A1: {
+      A11: {
+				A111: {
+					subSort: 0,
+          subSortKey: "A111"
+        },
+        A112: {
+					subSort: 0,
+					subSortKey: "A111"
+        },
+				subSort: 1,
+				subSortKey: A11
+      },
+      A12: {
 
+      },
+      A13: {
+
+      },
+      subSort: 1,
+      subSortKey: "A1"
+    },
+    A2: {
+
+    },
+    A3: {
+
+    },
+    subSort: 1,
+    subSortKey: "A"
+  },
+  B: {
+
+  },
+  C: {
+
+  }
+};
 App = React.createClass({
     render: function () {
         return <div>
-            <Header />
-            <div className="container content">
-                <Posts />
-            </div>
+            <ul>
+
+
+						</ul>
         </div>;
     }
 });
