1def9db3ef31db25e08e1f51dc6290d5adb67cc4
qhu5 <quentinhu5@gmail.com>
2015-07-02 23:03:30 +0000

    update clc-react

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,19 @@
 var React = require("react");
 var clc = require("./clc");
+var ClcList = require("./clcList");
 var Manipulate = require("./manipulate");
 var App;
 
 App = React.createClass({
+    getInitialState: function() {
+      return {
+        level: 0,
+        sortKey: ""
+      };
+    },
     render: function () {
       return <div>
-          <ul>
-            <li>test</li>
-					</ul>
+          <ClcList clc={this.props.clc}/>
           <Manipulate />
       </div>;
     }
--- assets/js/manipulate.js
+++ assets/js/manipulate.js
@@ -1,10 +1,13 @@
 var React = require("react");
 var Manipulate;
+
 Manipulate = React.createClass({
   render: function() {
     return <div className="manipulate">
-
+      <a href="#">&lt;</a>
+      <a href="#">&gt;</a>
     </div>;
   }
 });
+
 module.exports = Manipulate;
