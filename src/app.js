edb2ccca667e0fbab8287e535e697ab20cf56f90
qhu5 <quentinhu5@gmail.com>
2016-04-24 22:41:38 +0000

    replace hash path with real path

--- src/app.js
+++ src/app.js
@@ -5,49 +5,77 @@ import ClcList from './components/ClcList';
 require('./css/main.css');
 
 class App extends React.Component {
+  static defaultProps = {
+    level: 0,
+    sortCode: 'all'
+  }
   static propTypes = {
+    level: PropTypes.number.isRequired,
+    sortCode: PropTypes.string.isRequired,
     clc: PropTypes.object.isRequired
   }
   constructor(props) {
     super(props);
 
-    const route = window.location.hash.substr(1);
-    const routePartials = route.replace(/^\//, '').split('/');
-    const level = +routePartials[0] || 0;
-    const sortCode = routePartials[1] || '';
     this.state = {
-      level: level,
-      sortCode: sortCode
+      level: props.level,
+      sortCode: props.sortCode,
+      shouldUpdatePath: true
     };
 
     this.handleItemClick = this.handleItemClick.bind(this);
   }
+
   componentDidMount() {
-    window.addEventListener('hashchange', () => {
-      this.setState(this.getRouterObj());
-    });
+    this.pathToState();
+    this.listenPopState();
+  }
+  componentDidUpdate() {
+    this.changePath();
   }
 
-  getRouterObj() {
-    const route = window.location.hash.substr(1);
-    const routePartials = route.replace(/^\//, '').split('/');
-    const level = +routePartials[0] || 0;
-    const sortCode = routePartials[1] || '';
-    return {
-      level: level,
-      sortCode: sortCode
-    };
-  }
   handleItemClick(sortCode) {
     const level = this.state.level;
-    window.location.hash = '#/' + (level + 1) + '/' + sortCode;
 
     // 0    1 - A    2 - A1    3 - A11
     this.setState({
       level: level + 1,
-      sortCode: sortCode
+      sortCode: sortCode,
+      shouldUpdatePath: true
     });
   }
+  pathToState() {
+    const url: URL = new URL(window.location.href);
+    const path: string = url.pathname;
+    if (path === '/') {
+      this.changePath();
+    } else {
+      const pathPartials = path.replace(/^\//, '').split('/');
+      const [level, sortCode] = pathPartials;
+      this.setState({
+        level: +level,
+        sortCode,
+        shouldUpdatePath: true
+      });
+    }
+  }
+  changePath() {
+    if (this.state.shouldUpdatePath) {
+      const newPath: string = '/' +
+        this.state.level + '/' +
+        this.state.sortCode;
+      window.history.pushState(this.state, 'history path', newPath);
+    }
+  }
+  listenPopState() {
+    window.addEventListener('popstate', (e) => {
+      this.setState({
+        ...e.state,
+        shouldUpdatePath: false
+      });
+    });
+  }
+
   render() {
     return (
       <div className="clc-wrapper">
